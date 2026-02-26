370c52006cb6c13e78fb4ca3742d94f4ffd6ec69
Davlee1 <davidlee21019@gmail.com>
2026-02-26 00:26:48 -0500

    more progress

--- models/wishlist.js
+++ models/wishlist.js
@@ -22,7 +22,7 @@ const WishlistSchema = new mongoose.Schema(
     },
     //1 = most wanted, 5 = least important
     priority: {
-      type: String,
+      type: Number,
       enum: ["1", "2", "3", "4", "5"],
       default: "5",
       
