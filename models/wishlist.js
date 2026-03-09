fdec997ce25ea9c18f1d784bbb8009dd3ae87485
Davlee1 <davidlee21019@gmail.com>
2026-03-09 00:07:17 -0400

    typo fix as well as model fix

--- models/wishlist.js
+++ models/wishlist.js
@@ -23,7 +23,7 @@ const WishlistSchema = new mongoose.Schema(
     //1 = most wanted, 5 = least important
     priority: {
       type: Number,
-      enum: ["1", "2", "3", "4", "5"],
+      enum: [1, 2, 3, 4, 5],
       default: "5",
       
     },
